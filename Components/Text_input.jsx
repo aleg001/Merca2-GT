a3bbe8c1345059da4f1096dabc0858e4e54f9de5
Gabriela Paola Contreras Guerra <60274435+Paola-Contreras@users.noreply.github.com>
2022-04-02 12:13:56 -0600

    Add files via upload

---
+++ Components/Text_input.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './textin.css';
+
+const Text =() => {
+  return (
+    <div className="App">
+        <input placeholder="Texto"></input>
+    </div>
+  );
+}
+
+export default Text;
