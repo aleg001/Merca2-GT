06e98ac88596d186eb22871d605bf04d20fd8494
Nes15442 <60157586+Nes15442@users.noreply.github.com>
2022-04-20 18:00:24 -0600

    Configuracion de React

---
+++ Frontend/webpack.config.js
@@ -0,0 +1,32 @@
+export default {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            }
+        ]
+    },
+    devServer: {
+        static: {
+            directory: 'dist'
+        }
+    }
+}
