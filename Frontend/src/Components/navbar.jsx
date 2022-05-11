21614807382a3e886a597f43cb5dd3ab2fc5edba
Ale Gómez <gom20347@uvg.edu.gt>
2022-05-11 16:30:30 -0600

    changed navbar

--- Frontend/src/Components/navbar.jsx
+++ Frontend/src/Components/navbar.jsx
@@ -1,28 +1,25 @@
 import React, { Component } from "react";
 import "./navbar.css";
 
-class navbar extends Component {
-  state = {};
-  render() {
-    return (
-      <div className="navbar">
-        <ul>
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">Tus Artículos</a>
-          </li>
-          <li>
-            <a href="">Historial</a>
-          </li>
-          <li>
-            <a href="">Perfil</a>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Tus Artículos</a>
+        </li>
+        <li>
+          <a href="">Historial</a>
+        </li>
+        <li>
+          <a href="">Perfil</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default navbar;
+export default Navbar;
--- Frontend/src/pages/Homepage.jsx
+++ Frontend/src/pages/Homepage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "../components/Header.jsx";
 import TextInput from "../components/TextInput.jsx";
 import setDocTitle from "../util/docTitle.js";
+import Navbar from "../components/Navbar.jsx";
+import itemInGrid from "../components/itemInGrid.jsx";
 
 const Homepage = () => {
   setDocTitle("Homepage");
@@ -9,6 +11,7 @@ const Homepage = () => {
   return (
     <div className="content">
       <Header title="Homepage" />
+      <Navbar></Navbar>
       <div className="main-content-login"></div>
     </div>
   );
