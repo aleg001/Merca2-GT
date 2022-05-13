4e99cf90cbb355e293e52621b88a6592c4267c11
Nes15442 <60157586+Nes15442@users.noreply.github.com>
2022-05-13 13:51:24 -0600

    Arreglso en HomePage

--- Frontend/src/pages/Homepage.jsx
+++ Frontend/src/pages/Homepage.jsx
@@ -5,48 +5,46 @@ import Header from "../components/Header.jsx";
 import ItemInGrid from "../components/ItemInGrid.jsx";
 
 const handleItemsHomepage = (setItems) => {
-    const requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-    };
+  const requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+  };
 
-    fetch("http://127.0.0.1:8000/getItems", requestOptions)
+  fetch("http://127.0.0.1:8000/getItems", requestOptions)
     .then(response => response.json())
     .then(result => setItems(result.items))
     .catch(error => console.log('error', error));
 }
 
 const Homepage = () => {
-    const [items, setItems] = React.useState();
-    useEffect(() => {
-        handleItemsHomepage(setItems)
-    }, [])
+  const [items, setItems] = React.useState();
+  useEffect(() => {
+    handleItemsHomepage(setItems)
+  }, [])
 
-    items && console.log(items)
+  items && console.log(items)
 
 
   return (
-    <div className="content">
-        <Header title="Homepage"/>
-        <div className="main-content-login"></div>
-        <div className="container">
-            <div>
-                {items && items.map((item, index) => (
-                    <ItemInGrid
-                        itemName = {item.nombre}
-                        itemLocation = {item.ubicacion}
-                        itemPrice = {item.precio}
-                        itemPublishDate = {item.post_time}
-                        itemImage = {item.image}
-                        key={index}
-                    />
-                ))}
-            </div>
-        </div>
-        <footer>
-            Merca2-GT | Tel: +502 5017 - 6509 | administracion@merca2gt.com
-        </footer>
+  <div className="content">
+    <Header title="Homepage"/>
+    <div className="main-content-login"></div>
+    <div className="container">
+      {items && items.map((item, index) => (
+        <ItemInGrid
+          itemName = {item.nombre}
+          itemLocation = {item.ubicacion}
+          itemPrice = {item.precio}
+          itemPublishDate = {item.post_time}
+          itemImage = {item.image}
+          key={index}
+        />
+      ))}
     </div>
+    <footer>
+        Merca2-GT | Tel: +502 5017 - 6509 | administracion@merca2gt.com
+    </footer>
+  </div>
   );
 };
 
