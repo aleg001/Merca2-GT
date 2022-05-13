e09d1c60da5abf04b8aba363857de291c35fefec
Paola De León <64100118+dele20361@users.noreply.github.com>
2022-05-13 16:41:43 -0600

    Changed font color

--- Frontend/src/Components/itemInGrid.jsx
+++ Frontend/src/Components/itemInGrid.jsx
@@ -10,18 +10,17 @@ const ItemInGrid = ({
 }) => {
   return (
     <div className="container_item">
-      <div className="container_item">
-        <figure className="fotositem_portada">
-          <img className="productImage" src={itemImage}/>
-        </figure>
-        <div className="contenidositem_ubicacionprecio">
-          <h4 className="ubicacion"> {itemLocation} </h4>
-          <h5 className="product_price"> {itemPrice} </h5>
-        </div>
-        <h3 className="product_name"> {itemName}</h3>
-        <div className="contenidositem_tiemporating">
-          <h6 className="hora_publicacion"> {itemPublishDate} </h6>
-        </div>
+      <figure className="fotositem_portada">
+        <img className="productImage" src={itemImage}/>
+      </figure>
+      <div className="contenidositem_ubicacionprecio">
+        <h4 className="ubicacion"> {itemLocation} </h4>
+        <h5 className="product_price">Q. {itemPrice} </h5>
+      </div>
+      <h3 className="product_name"> {itemName}</h3>
+      <div className="contenidositem_tiemporating">
+        <h6 className="hora_publicacion">{itemPublishDate} </h6>
+        {/* <h3 className="hora_publicacion">Publicado el {itemPublishDate[8]}{itemPublishDate[9]}/{itemPublishDate[5]}{itemPublishDate[6]}/{itemPublishDate[0]}{itemPublishDate[1]}{itemPublishDate[2]}{itemPublishDate[3]}</h3> */}
       </div>
     </div>
   );
