a56341f38d39e5e2e467f550bff4aa06269af9c2
Nes15442 <60157586+Nes15442@users.noreply.github.com>
2022-08-10 18:17:29 -0600

    Eslint Config

---
+++ Frontend/.eslintrc.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    semi: [2, 'never'],
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': 0,
+    'linebreak-style': 0,
+    'import/extensions': [2, {
+      jsx: 'always',
+    }],
+    'react/function-component-definition': [2, {
+      namedComponents: 'arrow-function',
+      namedComponents: 'function',
+    }],
+    'import/no-extraneous-dependencies': [0, {
+      devDependencies: ['**/*.prop-types.*'],
+      peerDependencies: true,
+    }],
+    'prefer-template': 0,
+    'arrow-parens': [0, 'as-needed'],
+    'no-console': [1, {
+      allow: ['warn', 'error'],
+    }],
+  },
+};
