4d591c416aa8fdf0a598888dced9a56738607bf7
Ale Gómez <gom20347@uvg.edu.gt>
2022-08-21 22:40:42 -0600

    Created Error page

---
+++ Frontend/src/pages/ErrorPage.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import Header from '../Components/Header.jsx'
+import setDocTitle from '../util/docTitle.js'
+
+const ErrorPage = () => {
+  setDocTitle('Error 404')
+
+  return (
+    <div className='content'>
+      <Header title='ERROR 404' />
+      <div className='main-content-login'>
+        <form>
+          <p>
+            Ha ocurrido un error. Por favor contactar a algún administrador.
+          </p>
+
+          <br />
+          <br />
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default ErrorPage
