f8b49a8e66028b890bbe66df04ce4c8f79a31415
MarcoJurado20308 <71940438+MarcoJurado20308@users.noreply.github.com>
2022-09-26 14:34:03 -0600

    add item button test

--- Frontend/src/tests/AddItemButton.test.js
+++ Frontend/src/tests/AddItemButton.test.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
 import AddItemButton from '../Components/AddItemButton.jsx'
-
 import { isTSAnyKeyword } from '@babel/types'
 
-it('Render sin crashear', () => {
-  const div = document.createElement('div')
-  createRoot(div).render(<AddItemButton />)
+describe('Cuando se presiona el boton de agregar producto', () => {
+  it('se muestra la pagina de ingreso de informacion del nuevo producto', async () => {
+    const setter = jest.fn()
+    const setIsRegis = jest.fn()<
+    render(<Homepage userName={'pao'} setOnShow={set} />)
+    // Elemento del boton
+    const botonADD = screen.getByTestId('botonagregartesting')
+
+    // User event de click al boton
+    await userEvent.click(botonADD)
+
+    // Encontrar nuevo elemento en la nueva página
+    expect(
+      await screen.findByText(/Agregar nuevo producto/)
+    ).toBeInTheDocument()
+
+    expect(setIsRegis).toBeCalledWith(true)
+  })
 })
