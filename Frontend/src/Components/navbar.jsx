030e765cb6c183f718529987442798a5bf412109
Ale Gómez <gom20347@uvg.edu.gt>
2022-10-10 13:06:05 -0600

    updated navbar options

--- Frontend/src/Components/navbar.jsx
+++ Frontend/src/Components/navbar.jsx
@@ -21,15 +21,13 @@ const Navbar = ({ setOnShow, Cat, cat1 }) => (
       >
         Tus Artículos
       </button>
-      {Perfil
-        && (
-          <button
-            onClick={() => setOnShow('Perfil')}
-            type='button'
-          >
-            Perfil
-          </button>
-        )}
+
+      <button
+        onClick={() => setOnShow('Stats')}
+        type='button'
+      >
+        Estadísticas
+      </button>
       <select
         data-testid="select-nav"
         onChange={(event) => cat1(event.target.value)}
