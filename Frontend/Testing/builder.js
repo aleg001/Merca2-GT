be0e07f9a29854f85c63917956616a24eb7a90e4
Ale Gómez <gom20347@uvg.edu.gt>
2022-11-06 18:37:58 -0600

    Co-authored-by: Diego Córdova <Nes15442@users.noreply.github.com> Co-authored-by: DE LEON MOLINA, ANA PAOLA <dele20361@uvg.edu.gt> Co-authored-by: MarcoJurado20308 <MarcoJurado20308@users.noreply.github.com>-side-runner

--- Frontend/Testing/builder.js
+++ Frontend/Testing/builder.js
@@ -3,35 +3,28 @@
 
 import { By, Key, Builder } from 'selenium-webdriver'
 
-async function example() {
-  let driver = await new Builder().forBrowser('chrome').build()
+async function testLogin() {
+  const driver = await new Builder().forBrowser('chrome').build()
   await driver.get('http://localhost:8080/')
-  await driver.findElement(By.name('q')).sendKeys('Hello World', Key.RETURN)
+
+  await driver
+    .findElement(By.cssSelector("input[id*='userInput']"))
+    .getAttribute('Usuario')
+    .sendKeys('pao')
+
+  console
+    .log(
+      driver
+        .findElement(By.cssSelector("input[id*='userInput']"))
+        .getAttribute('Usuario')
+    )
+    .sendKeys('pao')
+
+  driver.find_element_by_class_name('btnLogin').click()
 
   setInterval(function () {
     driver.quit()
   }, 10000)
 }
 
-/*
-async function testLogin() {
-    let driver = await new Builder().forBrowser('chrome').build()
-    await driver.get("http://localhost:8080/")
-
-    WebElement caja_usuario = findElement_by_xpath("//a[@placeholderText='Usuario']")
-    caja_usuario.sendKeys('pao')
-
-    WebElement caja_usuario = findElement_by_xpath("//a[@placeholderText='Contraseña']")
-    caja_usuario.sendKeys('pao')
-
-    driver.find_element_by_class_name('btnLogin').click()
-
-
-    await driver.findElements(By.xpath("//ul[@placeholderText='Usuario']").sendKeys('Pao'))
-    await driver.findElements(By.xpath("//ul[@title='Ingrese su contraseña']").sendKeys('Pao'))
-    await 
-
-}
-*/
-
-example()
+testLogin()
