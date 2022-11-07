c1c06f647d216c91472c1fd0f2b0f2825cbe4217
Ale Gómez <gom20347@uvg.edu.gt>
2022-11-06 20:05:38 -0600

    Co-authored-by: Diego Córdova <Nes15442@users.noreply.github.com> Co-authored-by: DE LEON MOLINA, ANA PAOLA <dele20361@uvg.edu.gt> Co-authored-by: MarcoJurado20308 <MarcoJurado20308@users.noreply.github.com> Co-authored-by: CONTRERAS GUERRA, GABRIELA PAOLA <con20213@uvg.edu.gt>

--- Frontend/Testing/builder.js
+++ Frontend/Testing/builder.js
@@ -2,13 +2,11 @@
 // import { Builder, Browser, By, Key, until } from 'selenium-webdriver'
 
 import { By, Key, Builder } from 'selenium-webdriver'
+import JavascriptExecutor from 'selenium-webdriver'
 
 async function testLogin() {
   const driver = await new Builder().forBrowser('chrome').build()
   await driver.get('http://localhost:8080/')
-
-  //*[@id="root"]/div/div/form/div/div[1]/input
-  //*[@id="root"]/div/div/form/div/div[2]/input
 
   await driver
     .findElement(By.xpath('//*[@id="root"]/div/div/form/div/div[1]/input'))
@@ -18,11 +16,217 @@ async function testLogin() {
     .findElement(By.xpath('//*[@id="root"]/div/div/form/div/div[2]/input'))
     .sendKeys('pao')
 
-  driver.findElement(By.xpath('//*[@id="root"]/div/div/form/button[1]')).click()
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/form/button[1]'))
+    .click()
 
   setInterval(function () {
     driver.quit()
+    driver.close()
+  }, 3000)
+}
+
+async function testHomepageMisProductos() {
+  const driver = await new Builder().forBrowser('chrome').build()
+  await driver.get('http://localhost:8080/')
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/form/div/div[1]/input'))
+    .sendKeys('pao')
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/form/div/div[2]/input'))
+    .sendKeys('pao')
+
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/form/button[1]'))
+    .click()
+
+  await new Promise((r) => setTimeout(r, 2000))
+
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div[2]/div/button[2]'))
+    .click()
+
+  setInterval(function () {
+    //driver.quit()
+    //driver.close()
+  }, 3000)
+}
+
+async function testEstadisticas() {
+  const driver = await new Builder().forBrowser('chrome').build()
+  await driver.get('http://localhost:8080/')
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/form/div/div[1]/input'))
+    .sendKeys('pao')
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/form/div/div[2]/input'))
+    .sendKeys('pao')
+
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/form/button[1]'))
+    .click()
+
+  await new Promise((r) => setTimeout(r, 2000))
+
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div[2]/div/button[3]'))
+    .click()
+
+  setInterval(function () {
+    //driver.quit()
+    //driver.close()
+  }, 3000)
+}
+
+async function testVerDetallesProducto() {
+  // Abrir página
+  const driver = await new Builder().forBrowser('chrome').build()
+  await driver.get('http://localhost:8080/')
+
+  // Iniciar sesión
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/form/div/div[1]/input'))
+    .sendKeys('pao')
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/form/div/div[2]/input'))
+    .sendKeys('pao')
+
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/form/button[1]'))
+    .click()
+
+  // Cargar homepage
+  await new Promise((r) => setTimeout(r, 2000))
+
+  // Seleccionar producto
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div[4]/div[2]'))
+    .click()
+
+  setInterval(function () {
+    driver.quit()
+    //driver.close()
   }, 10000)
 }
 
-testLogin()
+async function testContactarProducto() {
+  // Abrir página
+  const driver = await new Builder().forBrowser('chrome').build()
+  await driver.get('http://localhost:8080/')
+
+  // Iniciar sesión
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/form/div/div[1]/input'))
+    .sendKeys('pao')
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/form/div/div[2]/input'))
+    .sendKeys('pao')
+
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/form/button[1]'))
+    .click()
+
+  // Cargar homepage
+  await new Promise((r) => setTimeout(r, 2000))
+
+  // Seleccionar producto
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div[4]/div[2]'))
+    .click()
+
+  // Cargar producto
+  await new Promise((r) => setTimeout(r, 2000))
+
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/div/div[4]/button[1]'))
+    .click()
+
+  setInterval(function () {
+    driver.quit()
+    //driver.close()
+  }, 10000)
+}
+
+async function testOfertarProducto() {
+  // Abrir página
+  const driver = await new Builder().forBrowser('chrome').build()
+  await driver.get('http://localhost:8080/')
+
+  // Iniciar sesión
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/form/div/div[1]/input'))
+    .sendKeys('pao')
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/form/div/div[2]/input'))
+    .sendKeys('pao')
+
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/form/button[1]'))
+    .click()
+
+  // Cargar homepage
+  await new Promise((r) => setTimeout(r, 2000))
+
+  // Seleccionar producto
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div[4]/div[2]'))
+    .click()
+
+  // Cargar producto
+  await new Promise((r) => setTimeout(r, 2000))
+
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/div/div[4]/button[2]'))
+    .click()
+
+  setInterval(function () {
+    driver.quit()
+    //driver.close()
+  }, 10000)
+}
+
+/*
+async function testSignUp() {
+  const driver = await new Builder().forBrowser('chrome').build()
+  await driver.get('http://localhost:8080/')
+
+  await driver
+    .findElement(By.xpath('//*[@id="root"]/div/div/form/button[2]'))
+    .click()
+
+  await new Promise((r) => setTimeout(r, 4000))
+
+  await driver
+    .findElement(By.xpath('//*[@id="form-singin"]/div/div[1]'))
+    .sendKeys('andrea1')
+
+  await driver
+    .findElement(By.xpath('//*[@id="form-singin"]/div/div[2]'))
+    .sendKeys('andrea')
+
+  await driver
+    .findElement(By.xpath('//*[@id="form-singin"]/div/div[3]'))
+    .sendKeys('grimaldi')
+
+  await driver
+    .findElement(By.xpath('//*[@id="form-singin"]/div/div[4]'))
+    .sendKeys('grim1@uvg.edu.gt')
+
+  await driver
+    .findElement(By.xpath(' //*[@id="form-singin"]/div/div[5]'))
+    .sendKeys('contraseña123')
+
+  await driver
+    .findElement(By.xpath('  //*[@id="form-singin"]/div/div[6]'))
+    .sendKeys('contraseña123')
+
+  await driver.findElement(By.xpath('//*[@id="date"]')).sendKeys('06/11/2022')
+
+  await driver.findElement(By.xpath('//*[@id="gender"]')).sendKeys('Femenino')
+
+  setInterval(function () {
+    driver.quit()
+    //driver.close()
+  }, 3000)
+}
+*/
