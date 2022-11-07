cd1f6ecd8debfc1517a48700adaa162a35dd35fe
Ale Gómez <gom20347@uvg.edu.gt>
2022-11-06 18:01:46 -0600

    Co-authored-by: Diego Córdova <Nes15442@users.noreply.github.com> Co-authored-by: DE LEON MOLINA, ANA PAOLA <dele20361@uvg.edu.gt> Co-authored-by: MarcoJurado20308 <MarcoJurado20308@users.noreply.github.com>

--- Frontend/Testing/builder.js
+++ Frontend/Testing/builder.js
@@ -5,13 +5,17 @@ import { By, Key, Builder } from 'selenium-webdriver'
 
 async function example() {
   let driver = await new Builder().forBrowser('chrome').build()
-  try {
-    await driver.get('https://www.google.com/ncr')
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
+  await driver.get('https://www.google.com/ncr')
+  await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
 
-    await driver.findElement(By.name('q')).sendKeys('Hello World')
-  } finally {
-    await driver.quit()
-  }
+  await driver.findElement(By.name('q')).sendKeys('Hello World', Key.RETURN)
+
+  setInterval(function () {
+    driver.quit()
+  }, 10000)
 }
+
+/*
+ */
+
 example()
